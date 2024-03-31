9acbc655f80dda15d40b41c42668ab4449af793c
sam <15692258a>
2024-03-31 23:44:26 +0800

    feat: add modify invoice feature

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,4 +1,5 @@
 export default function Page() {
+    const timer = () => new Promise(resolve => setTimeout(() => resolve(true), 1000));
     return (
         <><div>Welcome to Customers</div></>
     )
